08062b177b721f010a7326b6b1d64d887aa0bca4
monirulislam12 <happymonir12@gmail.com>
2020-07-06 16:36:41 +0600

    index updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,33 @@
 			nav:true,
 			navText: ["<img src='assets/img/arrow-left.png'>", "<img src='assets/img/arrow-right.png'>"],
 			dots:false,
-			autoplay:true
+			autoplay:true,
+			responsive: {
+                0: {
+                    items: 2,
+                },
+                767: {
+                    items: 2,
+                }
+            }
+		});
+
+		jQuery('nav').meanmenu({
+			meanMenuContainer: '.mobile-menu-container',
+			meanScreenWidth: "993"
+		});
+
+		$(".scroll-top").click(function () {
+			$("html,body").animate({
+				scrollTop: 0
+			}, 1000);
+		});
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 180) {
+				$(".scroll-top").fadeIn();
+			} else {
+				$(".scroll-top").fadeOut();
+			}
 		});
 
 
